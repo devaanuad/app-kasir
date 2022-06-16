e62051dff6ab2f71dbb656a4872db1c95a8cc2ae
devanuad <arkaandevaanu@gmail.com>
2022-06-16 17:01:46 +0700

    add cart

--- src/components/Cards/CardMenu.js
+++ src/components/Cards/CardMenu.js
@@ -1,19 +1,19 @@
 import { CartIcon } from "../../icons";
 import React from "react";
 
-function CardMenu({ title, src, onAdd }) {
-  return (
-    <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 mt-3">
+function CardMenu({ menu, onAdd }) {
+  return menu.map((item) => (
+    <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 mt-3 mb-3 w-40">
       <div className="px-4 py-2">
         <h1 className="text-3md text-center font-bold text-gray-800 uppercase dark:text-white">
-          {title}
+          {item.nama_menu}
         </h1>
       </div>
-      <img className="object-cover w-full h-32 mt-2" src={src} />
+      <img className="object-cover w-full h-32 mt-2" src={item.gambar} />
       <div className="flex items-center justify-between px-4 py-2 dark:bg-gray-900">
-        <h1 className="text-xs font-bold dark:text-white">Rp. 20.000</h1>
+        <h1 className="text-xs font-bold dark:text-white">Rp 20.000</h1>
         <button
-          onClick={() => onAdd()}
+          onClick={() => onAdd(item)}
           title="Tambah ke Cart"
           className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-200 transform bg-purple-600 rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none"
         >
@@ -21,7 +21,7 @@ function CardMenu({ title, src, onAdd }) {
         </button>
       </div>
     </div>
-  );
+  ));
 }
 
 export default CardMenu;
--- src/pages/kasir/transaksi.js
+++ src/pages/kasir/transaksi.js
@@ -2,24 +2,11 @@ import React, { useState, useEffect } from "react";
 import PageTitle from "../../components/Typography/PageTitle";
 import UsersAccess from "../../components/Middleware/BlockUsers";
 import {
+  Pagination,
+  TableFooter,
   Table,
   TableHeader,
-  TableCell,
-  TableBody,
-  TableRow,
-  TableFooter,
-  TableContainer,
-  Button,
-  Pagination,
   Input,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Label,
-  Select,
-  Badge,
-  Avatar,
 } from "@windmill/react-ui";
 import { EditIcon, TrashIcon, SearchIcon, CartIcon } from "../../icons";
 import axios from "axios";
@@ -31,31 +18,134 @@ import CardMenu from "../../components/Cards/CardMenu";
 function Transaksi() {
   // block login and akses role
   UsersAccess("kasir");
-  const titlee = "ayam Soto";
-  const srcc =
-    "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=320&q=80";
 
-  const onAddd = () => {
-    Swal.fire({
-      title: "Tambah Data",
+  const [dataMenu, setDataMenu] = useState([]);
+  const [dataMenu2, setDataMenu2] = useState(1);
+  const [dataMenu3, setDataMenu3] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const resultsPerPage = 5;
+  const totalResults = dataMenu.length;
+
+  function onPageChangeTable2(p) {
+    setDataMenu2(p);
+  }
+
+  useEffect(() => {
+    getDataMenu();
+  }, []);
+
+  const getDataMenu = async () => {
+    const response = await axios.get(API_URL + "api/kasir/menu", {
+      withCredentials: true,
+      headers: {
+        Authorization: `${localStorage.getItem("token")}`,
+      },
     });
+    setDataMenu(response.data.data);
+    setLoading(true);
   };
+
+  useEffect(() => {
+    if (dataMenu.length > 0) {
+      setDataMenu3(
+        dataMenu.slice(
+          (dataMenu2 - 1) * resultsPerPage,
+          dataMenu2 * resultsPerPage
+        )
+      );
+    }
+  }, [dataMenu]);
+
+  useEffect(() => {
+    setDataMenu3(
+      dataMenu.slice(
+        (dataMenu2 - 1) * resultsPerPage,
+        dataMenu2 * resultsPerPage
+      )
+    );
+  }, [dataMenu2]);
+
+  // search
+  useEffect(() => {
+    if (search !== "") {
+      setDataMenu3(
+        dataMenu.filter((menu) =>
+          menu.nama_menu.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    } else if (search === "") {
+      setDataMenu3(
+        dataMenu.slice(
+          (dataMenu2 - 1) * resultsPerPage,
+          dataMenu2 * resultsPerPage
+        )
+      );
+    }
+  }, [search]);
+
+  // cart keranjanggggggggg
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
+  console.log(cartItems);
 
   return (
     <>
       <PageTitle>Transaksi Baru</PageTitle>
-      <div className="flex flex-wrap">
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
-        <CardMenu title={titlee} src={srcc} onAdd={onAddd} />
+      <div className="grid gap-3 md:grid-cols-3 xl:grid-cols-3  w-xl mb-5">
+        <div className="relative focus-within:text-purple-500">
+          <div className="absolute inset-y-0 flex items-center pl-2">
+            <SearchIcon className="w-4 h-4" aria-hidden="true" />
+          </div>
+          <Input
+            type="text"
+            className="pl-8 text-gray-700"
+            placeholder="Search Menu"
+            aria-label="Search"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
       </div>
+      <Table>
+        <TableHeader>
+          <h1 className="text-md font-bold dark:text-white py-2 px-3 ">
+            List Menu
+          </h1>
+        </TableHeader>
+        {loading ? (
+          <div className="flex flex-wrap gap-10">
+            <CardMenu menu={dataMenu3} onAdd={onAdd} />
+          </div>
+        ) : (
+          <div className="pos-center">
+            <div className="loader" />
+          </div>
+        )}
+        <TableFooter>
+          <Pagination
+            totalResults={totalResults}
+            resultsPerPage={resultsPerPage}
+            onChange={onPageChangeTable2}
+            label="Table navigation"
+          />
+        </TableFooter>
+      </Table>
     </>
   );
 }
